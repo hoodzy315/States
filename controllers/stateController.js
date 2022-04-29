c773c64ebef1790c7fbc35cce3143a7355680c6b
hoodzy315 <jjwoods2@mail.fhsu.edu>
2022-04-29 14:49:11 -0700

    fix

--- controllers/stateController.js
+++ controllers/stateController.js
@@ -13,7 +13,7 @@ const getAllStates = async (req, res) => {
             const element = contig[i];
             const funfact = await States.findOne({stateCode: element.code}).lean();
             if(funfact){
-                const merged = {...element, "funfacts": funfact.funfacts};
+                const merged = {...element, "funfacts": `${funfact.funfacts}`};
                 full.push(merged);
             }
             else {
@@ -27,7 +27,7 @@ const getAllStates = async (req, res) => {
             const element = nocontig[i];
             const funfact = await States.findOne({stateCode: element.code}).lean();
             if(funfact){
-                const merged = {...element, "funfacts": funfact.funfacts};
+                const merged = {...element, "funfacts": `${funfact.funfacts}`};
                 full.push(merged);
             }
             else {
@@ -40,7 +40,7 @@ const getAllStates = async (req, res) => {
             const element = data.states[i];
             const funfact = await States.findOne({stateCode: element.code}).lean();
             if(funfact){
-                const merged = {...element, "funfacts": funfact.funfacts};
+                const merged = {...element, "funfacts": `${funfact.funfacts}`};
                 full.push(merged);
             }
             else {
@@ -135,7 +135,7 @@ const updateFact = async (req,res) => {
         return res.status(400).json({ 'message': "State fun fact index value required" });
     }
     if (!req?.body?.funfact) {
-        return res.status(400).json({ 'message': 'State fun facts value required' });
+        return res.status(400).json({ 'message': 'State fun fact value required' });
     }
     if(!Array.isArray(req.body.funfact)){
         return res.status(400).json({"message": 'State fun fact value required'});
